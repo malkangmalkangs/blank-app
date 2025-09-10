9ecc6fd3629d277e3f663ff0179c03d34757b552
malkangmalkangs <malkang.yang@gmail.com>
2025-09-10 08:31:05 +0000

    제목 하단 문구 수정

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 hello minkang")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "------------------------------------------------------------------------------------"
 )
